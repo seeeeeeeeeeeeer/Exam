062c31f2700170cb0e556da5c5b54da39a343b76
Dmytro <=>
2022-02-03 16:22:09 +0200

    sgfhsfgtjndm

--- Exam/Source.cpp
+++ Exam/Source.cpp
@@ -1,26 +1,50 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<fstream>
 
 using namespace std;
 
 class guest
 {
+private:
+	string login;
+	string password;
 	string name;
 	string surname;
 	double rating;
+	int marks[12]{};
 public:
 	guest()
 	{
+		login = "NULL";
+		password = "NULL";
 		name = "NULL";
 		surname = "NULL";
 		rating = 0;
 	}
-	guest()
+	guest(string login,string password,string name,string surname,double rating,int* marks)
 	{
+		this->login = login;
+		this->password = password;
 		this->name = name;
 		this->surname = surname;
 		this->rating = rating;
+		this->marks[12];
+	}
+	guest(string login, string password, string name, string surname, double rating, int* marks)
+	{
+		for (int i = 0; i < 12; i++)
+		{
+			this->marks[i] = marks[i];
+		}
+	}
+	guest(string login, string password, string name, string surname, double rating, int* marks)
+	{
+		for (int i = 0; i < 12; i++)
+		{
+			cout << "Marks: "<< marks[i];
+		}
 	}
 	void print()
 	{
@@ -29,9 +53,22 @@ public:
 		cout << "Rating: "<<"\n";
 		cin >> rating;
 	}
+	string get_login() { return login; }
+	string get_password() { return password; }
 	string get_name() { return name; }
 	string get_surname() { return surname; }
-	double get_rating() { return rating; }
+	double get_rating() { return rating; }	
+	void set_login()
+	{
+		cout << "Enter Login: ";
+		cin >> login;
+		if(login!=)
+	}
+	void set_password()
+	{
+		cout << "Enter Password: ";
+		cin >> password;
+	}
 	void set_name()
 	{
 		cout << "Enter Name: ";
@@ -49,13 +86,42 @@ public:
 	}
 };
 
-class admin
+class admin:private guest
 {
+
+	string login;
+	string password;
 	string guests;
 public:
-	void guests() { guests = "NULL"; }
-	void guests() { this->guests = guests; }
-	
+	admin()
+	{
+		login = "NULL";
+		password = "NULL";
+		guests = "NULL";
+	}
+	admin(string login,string password,string guests)
+	{
+		this->login = login;
+		this->password = password;
+		this->guests = guests;
+	}
+	string get_login() { return login; }
+	string get_password() { return password; }
+	string get_guests() { return guests; }
+	void set_login()
+	{
+		cout << "Enter login: ";
+		cin >> login;
+		if (login!= login)
+		{
+			cout<<""
+		}
+	}
+	void set_password()
+	{
+		cout << "Enter password: ";
+		cin >> password;
+	}
 };
 
 class system:public guest,public admin
@@ -63,24 +129,18 @@ class system:public guest,public admin
 	string categories;
 	string test;
 	string ask_to_test;
-	string login;
-	string password;
 public:
 	system()
 	{
 		categories = "NULL";
 		test = "NULL";
-		ask_to_test = "NULL";
-		login = "NULL";
-		password = "NULL";
+		ask_to_test = "NULL";		
 	}
-	system(string name, string surname, double rating,string login, string password, string categories, string test)
+	system(string name, string surname, double rating, string categories, string test)
 	{
 		this->categories = categories;
 		this->test = test;
-		this->ask_to_test = ask_to_test;
-		this->login = login;
-		this->password = password;
+		this->ask_to_test = ask_to_test;	
 	}
 	void print()
 	{
@@ -91,32 +151,61 @@ public:
 	}
 	string get_categories() { return categories; }
 	string get_test() { return test; }
-	string get_ask_to_test() { return ask_to_test; }
-	string login() { return login; }
-	string password(){ return password; }
+	string get_ask_to_test() { return ask_to_test; }	
 	void set_categories()
-	{
-		cout << "Enter Login: ";
-		cin >> login;
-		cout << "Enter Password: ";
-		cin >> password;
+	{	
 		cout << "Choose you categories: ";
 		cin >> categories;
 		cout << "Choose you test: ";
 		cin >> test;
 	}
+	void save_to_file()
+	{
 
+	}
+	void write_from_file()
+	{
+
+	}
+	void entering_system()
+	{
+	}
 };
 
 class test
 {
 	string question;
 	string answer;
+public:
+	test()
+	{
+		question = "NULL";
+		answer = "NULL";
+	}
+	test(string question, string answer)
+	{
+		this->question = question;
+		this->answer = answer;
+	}
+	void print()
+	{
+		cout << "Question :" << question << "\n";
+		cout << "Answer is: " << answer << "\n";
+	}
+	string get_question() { return question; }
+	string get_answer() { return answer; }
+	void set_answer()
+	{
+		cout << "Youre answer? ";
+		cin >> answer;
+	}
 };
 
 int main()
 {
-
+	guest g;
+	
+	g.print();
 
 	return 0;
 }
